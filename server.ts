40e0f2f14373a2de8caf49f2785f903b77094898
Grace Zaborski <grace.zaborski@beamery.com>
2021-12-06 20:06:47 +0000

    Adding remote server info and connecting to local server

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import { Client } from "pg";
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
+const client = require("./db");
 
 config(); //Read .env file lines as though they were env vars.
 
@@ -23,12 +24,17 @@ const app = express();
 app.use(express.json()); //add body parser to each following route handler
 app.use(cors()) //add CORS support to each following route handler
 
-const client = new Client(dbConfig);
-client.connect();
+// const client = new Client(dbConfig);
+// client.connect();
 
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
-  res.json(dbres.rows);
+  try {
+    const dbres = await client.query('select * from rbgdatabase');
+    res.json(dbres.rows);
+  }
+  catch (error) {
+    console.error(error.message)
+  }
 });
 
 
