43e0069ae866daedf0b78775c7311946aca6a18e
Grace Zaborski <grace.zaborski@beamery.com>
2021-12-06 20:27:32 +0000

    Ready for heroku

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import { Client } from "pg";
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
-const client = require("./db");
+// const client = require("./db");
 
 config(); //Read .env file lines as though they were env vars.
 
@@ -24,12 +24,12 @@ const app = express();
 app.use(express.json()); //add body parser to each following route handler
 app.use(cors()) //add CORS support to each following route handler
 
-// const client = new Client(dbConfig);
-// client.connect();
+const client = new Client(dbConfig);
+client.connect();
 
 app.get("/", async (req, res) => {
   try {
-    const dbres = await client.query('select * from rbgdatabase');
+    const dbres = await client.query('select * from pastebindb');
     res.json(dbres.rows);
   }
   catch (error) {
